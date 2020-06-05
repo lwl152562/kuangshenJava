d1f29d15962c3220cc6471a07517b5b193b2ed4d
wang <710288244@qq.com>
2020-06-05 08:56:45 +0800

    线程优先级

---
+++ ThreadLesson/src/com/kuang/state/TestPriority.java
@@ -0,0 +1,42 @@
+package com.kuang.state;
+
+
+// 优先级默认为5
+public class TestPriority extends Thread{
+    public static void main(String[] args) {
+
+        System.out.println(Thread.currentThread().getName()+"-->"+Thread.currentThread().getPriority());
+
+        MyPriority myPriority = new MyPriority();
+
+        Thread t1 = new Thread(myPriority);
+        Thread t2 = new Thread(myPriority);
+        Thread t3 = new Thread(myPriority);
+        Thread t4 = new Thread(myPriority);
+        Thread t5 = new Thread(myPriority);
+        Thread t6 = new Thread(myPriority);
+
+        //先设置优先级再启动
+        t1.start();
+        t2.setPriority(1);
+        t2.start();
+        t3.setPriority(4);
+        t3.start();
+        t4.setPriority(Thread.MAX_PRIORITY);
+        t4.start();
+        t5.setPriority(8);
+        t5.start();
+        t6.setPriority(7);
+        t6.start();
+
+
+    }
+}
+
+
+class MyPriority implements Runnable{
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName()+"-->" + Thread.currentThread().getPriority());
+    }
+}
