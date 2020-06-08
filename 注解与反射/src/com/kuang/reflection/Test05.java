7d421a3699beb0c192f6126c3c6f58d6b6b6e000
wang <710288244@qq.com>
2020-06-08 21:24:53 +0800

    类加载分析

---
+++ 注解与反射/src/com/kuang/reflection/Test05.java
@@ -0,0 +1,23 @@
+package com.kuang.reflection;
+
+import com.sun.org.glassfish.external.statistics.impl.StatisticImpl;
+
+public class Test05 {
+    public static void main(String[] args) {
+        A a = new A();
+        System.out.println(A.m);
+    }
+}
+
+class A{
+    static {
+        System.out.println("A类的静态代码块和初始化");
+        m = 300;
+    }
+
+    static int m = 100;
+
+    public A() {
+        System.out.println("A类的无参构造初始化");
+    }
+}
